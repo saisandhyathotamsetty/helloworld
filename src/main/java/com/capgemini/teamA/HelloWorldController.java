00c2d89e286c019891b87d5198347223ff3403a7
mohitchandra19 <mohitnanduri7@gmail.com>
2022-03-07 19:00:09 +0530

    Added a comment

--- src/main/java/com/capgemini/teamA/HelloWorldController.java
+++ src/main/java/com/capgemini/teamA/HelloWorldController.java
@@ -10,6 +10,9 @@ public class HelloWorldController {
 	//http://localhost:8080/hw
 	
 	@GetMapping("/hw")
+	/*
+	 * Added a comment and pushing it to github account
+	 */
 	public String helloworld() {
 		return "Hello World";
 
